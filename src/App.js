1ca5c9c4a43e6eedf9e3226263ef5d9020debbef
1radmila2 <148565773+1radmila2@users.noreply.github.com>
2024-01-22 13:03:41 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { DashBoard } from './components/DashBoard';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DashBoard />
+      </div>
+    );
+  }
+}
+
+export default App;
